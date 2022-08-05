6f75c483d548259059308a4fb3ae76c928071373
000VIJAY <vijay893689@gmail.com>
2022-08-05 19:02:48 +0530

    Vijay added program for validate pin code

--- ValidatePinCode.js
+++ ValidatePinCode.js
@@ -1,1 +1,9 @@
-console.log("Welcome to User validation Program");
+let validRegex = RegExp('^[1-9]{1}[0-9]{5}$')
+let pincode = 801306;
+function validFunc()
+{
+    if(validRegex.test(pincode))
+    {console.log("Valid pin code")}
+    else{throw 'Invalid pincode ';}
+}
+validFunc();
