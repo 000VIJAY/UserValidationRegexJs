ba4a6509c5abbada49f2909c44f5ffb09cd57522
000VIJAY <vijay893689@gmail.com>
2022-08-05 19:25:35 +0530

    Vijay added program for validate email id

--- ValidatePinCode.js
+++ ValidatePinCode.js
@@ -1,5 +1,7 @@
 let validRegex = RegExp('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$')
+let validEmail = RegExp('^[A-Za-z0-9._]+@[A-Za-z0-9]+.[a-z]{2,5}$')
 let pincode = "804 306";
+let email = "Vijayk@gmail.co";
 function validFunc()
 {
     if(validRegex.test(pincode))
@@ -7,3 +9,10 @@ function validFunc()
     else{throw 'Invalid pincode ';}
 }
 validFunc();
+function validEmailFunc()
+{
+    if(validEmail.test(email))
+    {console.log("Valid email Id")}
+    else{throw 'Invalid email id ';}
+}
+validEmailFunc();
